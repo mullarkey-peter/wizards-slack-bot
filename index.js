e2cc5c434a5f0520402c5fd0dbfe7910fc54b51b
mullarkey-peter <peter.mullarkey98@gmail.com>
2022-02-11 20:35:31 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('cross-fetch');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/wizards-never-die', (req, res) => {
+  res.send('************* Wizards Never Die!');
+});
+
+app.post('/wizards-never-die/schedule', function (req, res) {
+    var apiUrl = `https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2021/league/00_full_schedule.json`
+    let today = new Date();
+
+    // Get full NBA schedule for the given year
+    fetch(apiUrl).then((resp) => {
+        return resp.json();
+    }).then((data) => {
+        var games = [];
+        for ( i in data.lscd) {
+            for( j in data.lscd[i].mscd.g) {
+                if(data.lscd[i].mscd.g[j].h.tn == 'Wizards') {
+                    var gameTime = new Date(data.lscd[i].mscd.g[j].gdtutc);
+                    if(gameTime >= today) {
+                        var gameData = {
+                            "time" : data.lscd[i].mscd.g[j].gdtutc,
+                            "opponent" : data.lscd[i].mscd.g[j].v.tn
+                        };
+                        games.push(gameData);
+                    }
+                }
+            }
+        }
+        res.status(200).send(games);
+    }).catch((error) => {
+        console.log(error);
+    });
+  });
+
+app.listen(port, () => {
+  console.log(`Slack Bot listening on port ${port}`);
+});
